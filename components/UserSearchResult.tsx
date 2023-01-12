568c05b07bd60e7e77f129ef5b7ba20ea4c54780
Glenn <phoobynet@users.noreply.github.com>
2023-01-12 16:28:39 +0000

    wip

--- components/UserSearchResult.tsx
+++ components/UserSearchResult.tsx
@@ -2,6 +2,7 @@ import styles from './UserSearchResult.module.scss'
 import { useAppStore } from '@/stores/useAppStore'
 import { useMemo } from 'react'
 import { format, parseISO } from 'date-fns'
+import IconCompany from '@/assets/icon-company.svg'
 
 export default function UserSearchResult () {
   const user = useAppStore(state => state.user)
@@ -51,6 +52,23 @@ export default function UserSearchResult () {
       </div>
 
       <div className={styles.bio}>{bio}</div>
+      <div className={styles.stats}>
+        <div className={styles.stat}>
+          <div>Repos</div>
+          <div>{user.public_repos}</div>
+        </div>
+        <div className={styles.stat}>
+          <div>Followers</div>
+          <div>{user.followers}</div>
+        </div>
+        <div className={styles.stat}>
+          <div>Following</div>
+          <div>{user.following}</div>
+        </div>
+      </div>
+      <div className={styles.links}>
+        {/*{IconCompany}*/}
+      </div>
     </div>
   )
 }
---
+++ components/UserSearchResultLink.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+
+interface Props {
+  value?: string
+  icon: ReactNode
+}
+
+export default function UserSearchResultLink ({}: Props) {
+  return (
+    <div></div>
+  )
+}
