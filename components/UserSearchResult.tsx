b3c29dc060b725d8d91b9ad873a0186519a1560a
Glenn <phoobynet@users.noreply.github.com>
2023-01-13 11:40:08 +0000

    wip

--- components/UserSearchResult.tsx
+++ components/UserSearchResult.tsx
@@ -1,12 +1,13 @@
 import styles from './UserSearchResult.module.scss'
 import { useAppStore } from '@/stores/useAppStore'
-import { ReactNode, useMemo } from 'react'
+import { useMemo } from 'react'
 import { format, parseISO } from 'date-fns'
 import IconCompany from '@/components/IconCompany'
 import IconTwitter from '@/components/IconTwitter'
 import IconLocation from '@/components/IconLocation'
 import IconWebsite from '@/components/IconWebsite'
 import { motion } from 'framer-motion'
+import UserSearchResultLink from '@/components/UserSearchResultLink'
 
 export default function UserSearchResult () {
   const user = useAppStore(state => state.user)
@@ -84,59 +85,26 @@ export default function UserSearchResult () {
         </div>
       </div>
       <div className={styles.links}>
-        <Link
+        <UserSearchResultLink
           icon={<IconLocation />}
           value={user.location}
         />
 
-        <Link
+        <UserSearchResultLink
           icon={<IconWebsite />}
           value={user.blog}
         />
 
-        <Link
+        <UserSearchResultLink
           icon={<IconTwitter />}
           value={user.twitter_username}
         />
 
-        <Link
+        <UserSearchResultLink
           icon={<IconCompany />}
           value={user.company}
         />
       </div>
     </motion.div>
-  )
-}
-
-function Link ({
-  icon,
-  value,
-}: { icon: ReactNode, value: string | undefined }) {
-  const notAvailable = !value
-  const isURL = notAvailable
-    ? false
-    : value?.toLowerCase().indexOf('http') === 0
-  return (
-    <>
-      <div
-        className={`${styles.icon} ${notAvailable
-          ? styles.notAvailable
-          : ''}`}
-      >
-        {icon}
-      </div>
-      <div
-        className={`${styles.value} ${notAvailable
-          ? styles.notAvailable
-          : ''}`}
-      >{
-        isURL
-          ? (<a
-            href={value}
-            target="_blank"
-            rel="noreferrer"
-          >{value}</a>)
-          : <span>{value}</span>}</div>
-    </>
   )
 }
--- components/UserSearchResultLink.tsx
+++ components/UserSearchResultLink.tsx
@@ -1,12 +1,41 @@
 import { ReactNode } from 'react'
+import styles from './UserSearchResultLink.module.scss'
 
 interface Props {
   value?: string
   icon: ReactNode
 }
 
-export default function UserSearchResultLink ({}: Props) {
+export default function UserSearchResultLink ({
+  icon,
+  value,
+}: Props) {
+  const notAvailable = !value
+  const isURL = notAvailable
+    ? false
+    : value?.toLowerCase().indexOf('http') === 0
+
   return (
-    <div></div>
+    <div className={styles.userLinkSearchResult}>
+      <div
+        className={`${styles.icon} ${notAvailable
+          ? styles.notAvailable
+          : ''}`}
+      >
+        {icon}
+      </div>
+      <div
+        className={`${styles.value} ${notAvailable
+          ? styles.notAvailable
+          : ''}`}
+      >{
+        isURL
+          ? (<a
+            href={value}
+            target="_blank"
+            rel="noreferrer"
+          >{value}</a>)
+          : <span>{value}</span>}</div>
+    </div>
   )
 }
